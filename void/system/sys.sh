a0e61da0ea3558b1af0f7b3f96ba6b0b135e6c50
Keyn73 <82419402+Keynag@users.noreply.github.com>
2024-07-12 16:50:33 +0300

    Update sys.sh

--- void/system/sys.sh
+++ void/system/sys.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo xbps-install -Su;
+sudo xbps-install void-repo-multilib && sudo xbps-install -Suy;
 
-sudo xbps-install linux-firmware-amd linux-firmware-intel xorg-server-xwayland xorg-server-xwayland-32bit wayland wlroots wayland-32bit wayland-protocols pcre2 json-c pango cairo meson zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting qt5-wayland qt6-wayland mesa mesa-32bit mesa-demos vulkan-tools Vulkan-ValidationLayers pipewire pipewire-32bit wireplumber pavucontrol zramen curl chrony seatd elogind;
+sudo xbps-install linux-firmware-amd linux-firmware-intel xorg-server-xwayland xorg-server-xwayland-32bit wayland wlroots wayland-32bit wayland-protocols pcre2 json-c pango cairo meson zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting qt5-wayland qt6-wayland mesa mesa-32bit mesa-demos vulkan-tools Vulkan-ValidationLayers pipewire pipewire-32bit wireplumber pavucontrol zramen curl chrony seatd elogind gdk-pixbuf gdk-pixbuf-32bit;
 
 sudo xbps-install noto-fonts-ttf dejavu-fonts-ttf google-fonts-ttf liberation-fonts-ttf font-awesome6 nerd-fonts-symbols-ttf;
 
-sudo xbps-install mesa-dri mesa-dri-32bit mesa-vulkan-radeon mesa-vulkan-radeon-32bit vulkan-loader-32bit vulkan-loader amdvlk amdvlk-32bit mesa-vaapi mesa-vaapi-32bit mesa-vdpau mesa-vdpau-32bit lrzip unrar unzip p7zip squashfs-tools engrampa mpv firefox feh openjdk-src openjdk timeshift wireguard wireguard-tools openresolv audacious audacious-plugins corectrl steam gparted bleachbit gnome-calculator telegram-desktop xdg-desktop-portal-gtk;
+sudo xbps-install mesa-dri mesa-dri-32bit mesa-vulkan-radeon mesa-vulkan-radeon-32bit vulkan-loader-32bit vulkan-loader amdvlk amdvlk-32bit mesa-vaapi mesa-vaapi-32bit mesa-vdpau mesa-vdpau-32bit lrzip unrar unzip p7zip squashfs-tools engrampa mpv firefox feh openjdk-src openjdk timeshift wireguard wireguard-tools openresolv audacious audacious-plugins corectrl steam gparted bleachbit gnome-calculator telegram-desktop xdg-desktop-portal-gtk Vulkan-Headers;
 
 sudo xbps-install wofi Waybar grim slurp mako alacritty pcmanfm transmission-gtk wl-clipboard gammastep pywal gsimplecal-gtk3 yad dbus swaybg polkit wob;
 
