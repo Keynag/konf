373ffa64803be1b21eb00c7f00e906d68840801d
Keyn73 <82419402+Keynag@users.noreply.github.com>
2024-07-15 15:46:19 +0300

    Update sys.sh

--- void/system/sys.sh
+++ void/system/sys.sh
@@ -12,7 +12,7 @@ sudo xbps-install wofi Waybar grim slurp mako alacritty pcmanfm transmission-gtk
 
 #sudo xbps-install chrony
 
-mkdir -p /etc/pipewire/pipewire.conf.d
+sudo mkdir -p /etc/pipewire/pipewire.conf.d
 sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
 sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 
